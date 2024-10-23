bee660d5193ad38903074a5a6b0f1df810f22e13
Brian Ball <bball@win.redhat.com>
2024-10-23 15:17:25 -0400

    Adds some base configuration to spin up MLflow.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # MLflow on OCP
 
 The objective of this project is to create an easy path to deploy MLflow on OpenShift.
+
+## Prerequisites
+
+This project assumes that you do not currently have MinIO installed.
+
+Installed operators:
+* Crunchy Operator (see `operators/crunchy_subscription.yaml`)
+* RHOAI
---
+++ deploy.sh
@@ -0,0 +1,15 @@
+oc apply -f manifests
+
+PASSWORD=$(oc get secret -n mlflow mlflow-postgres-pguser-mlflowuser -o json | jq -r .data.password | tr -d '\n' | tr -d '\r\n' | base64 -d)
+AWSACCESSKEYID=$(oc get secret -n minio aws-connection-my-storage -o json | jq -r .data.AWS_ACCESS_KEY_ID | tr -d '\n' | tr -d '\r\n' | base64 -d)
+AWSSECRETACCESSKEY=$(oc get secret -n minio aws-connection-my-storage -o json | jq -r .data.AWS_SECRET_ACCESS_KEY | tr -d '\n' | tr -d '\r\n' | base64 -d)
+
+# TODO: Make this cleaner for folks who may or may not want this repository with this name
+helm repo add community-charts https://community-charts.github.io/helm-charts
+helm repo update
+
+helm install -n mlflow mlflow -f helm/mlflow_values.yaml \
+ --set artifactRoot.s3.awsAccessKeyId=$AWSACCESSKEYID \
+ --set artifactRoot.s3.awsSecretAccessKey=$AWSSECRETACCESSKEY \
+ --set backendStore.postgres.password=$PASSWORD \
+  community-charts/mlflow
